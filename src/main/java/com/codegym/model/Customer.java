e9b1f45e691df51538b7eb8194b40d4495df90f7
khoa1812 <khoatrinh2909@gmail.com>
2024-05-31 08:35:24 +0700

    1

---
+++ src/main/java/com/codegym/model/Customer.java
@@ -0,0 +1,16 @@
+package com.codegym.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "customer")
+@Data
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
